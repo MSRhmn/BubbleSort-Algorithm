3eb49ccadd74f21ed8c3bfe0a31fc4f0cfe5f2bf
MS Rahman <msrhmn@outlook.com>
2020-05-20 18:34:55 +0600

    Added empty line in the last row

--- bubbleSort.js
+++ bubbleSort.js
@@ -5,8 +5,8 @@ function bubbleSort(arr) {
   for(let i = arr.length; i > 0; i--) {
     for(let j = 0; j < i; j++) {
       if(arr[j] > arr[j + 1]) {
-        temp = arr[j];
-        arr[j] = arr[j + 1];
+        temp       = arr[j];
+        arr[j]     = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
