d00bd907b30f3f669f47af514a410df2a38319a1
MS Rahman <msrhmn@outlook.com>
2020-05-20 18:33:30 +0600

    Added bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+let arr = [5, 3, 1, 9, 7, 11, 2, 19, 21, 13];
+
+function bubbleSort(arr) {
+  let temp;
+  for(let i = arr.length; i > 0; i--) {
+    for(let j = 0; j < i; j++) {
+      if(arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(arr));
