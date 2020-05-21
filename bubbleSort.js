3b2e1b4cdc912499828da7881811b4d1083c71e4
MS Rahman <msrhmn@outlook.com>
2020-05-21 12:38:10 +0600

    Added ES6 standard syntax

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,6 +1,6 @@
-let arr = [5, 3, 1, 9, 7, 11, 2, 19, 21, 13];
+const arr = [5, 3, 1, 9, 7, 11, 2, 19, 21, 13];
 
-function bubbleSort(arr) {
+const bubbleSort = (arr) => {
   let temp;
   for(let i = arr.length; i > 0; i--) {
     for(let j = 0; j < i; j++) {
